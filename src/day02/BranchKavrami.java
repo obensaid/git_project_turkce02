538cab6b46a2887cc05ab3d5a8003ddf5950cd59
obensaid <obenyilmaz@gmail.com>
2020-08-16 18:18:55 +0200

    enson hali

--- src/day02/BranchKavrami.java
+++ src/day02/BranchKavrami.java
@@ -3,13 +3,8 @@ package day02;
 public class BranchKavrami {
 
 	public static void main(String[] args) {
-
-		
-		
 		
 		System.out.println("16:53 - master Branch");
-
-
 		System.out.println("17:13 - oben Branch");
 
 	}
